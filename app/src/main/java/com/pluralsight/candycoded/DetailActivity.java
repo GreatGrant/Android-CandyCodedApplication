4adfcb1f37754f4abf95477d33a9a2e127f6809b
Stephen Odumirin <Stephenodumirin@gmail.com>
2020-09-24 16:12:29 +0100

    New changes

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -69,7 +69,8 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void createShareIntent(){
-       Intent shareIntent = new Intent(Intent.ACTION_SEND);
+       Intent shareIntent = new Intent();
+       shareIntent.setAction(Intent.ACTION_SEND);
        shareIntent.setType("text/plain");
        shareIntent.putExtra(Intent.EXTRA_TEXT,"Share Intent" + SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED);
        startActivity(shareIntent);
